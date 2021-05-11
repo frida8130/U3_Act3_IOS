356435dac8b66e073a8cb32f59ac5a7b8a455905
FlutLab.io <>
2021-05-11 18:20:06 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MiFilaColumnaApp());
+
+class MiFilaColumnaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Filas Y Columnas",
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: PaginaInicial(),
+    ); //Fin Material
+  } //build
+} //Fin Classmifilacolumna
+
+class PaginaInicial extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Filas y Columnas de Angelica'),
+      ), //fin de appbar
+      body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Container(
+            //zona verde
+            color: Colors.greenAccent[400],
+            width: 1000,
+            height: 570,
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                //contenedor dentro del children
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  color: Colors.pink,
+                  width: 500,
+                  height: 75,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        color: Colors.grey,
+                        width: 60,
+                        height: 55,
+                      ), //fin container 
+                      SizedBox(width: 16),
+                      Container(
+                        color: Colors.purple,
+                        width: 60,
+                        height: 55,
+                      ), //fin container 
+                      SizedBox(width: 16),
+                      Container(
+                        color: Colors.blue,
+                        width: 60,
+                        height: 55,
+                      ), //fin container 
+                    ], //fin del widget[]
+                  ), //fin del row fin de fila 1
+                ), //fin del container naranja
+
+                SizedBox(height: 16),
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  color: Colors.white,
+                  width:500,
+                  height: 75,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(color: Colors.red, width: 60, height: 55),
+                      SizedBox(width: 16),
+                      Container(color: Colors.blue, width: 60, height: 55),
+                      SizedBox(width: 16),
+                      Container(color: Colors.black, width: 60, height: 55),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 16),
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  color: Colors.blue,
+                  width: 500,
+                  height: 75,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(color: Colors.pink, width: 60, height: 55),
+                      SizedBox(width: 16),
+                      Container(color: Colors.yellow, width: 60, height: 55),
+                      SizedBox(width: 16),
+                      Container(color: Colors.green, width: 60, height: 55),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 16),
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  color: Colors.orange,
+                  width: 500,
+                  height: 75,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(color: Colors.green, width: 60, height: 55),
+                      SizedBox(width: 16),
+                      Container(color: Colors.blue, width: 60, height: 55),
+                      SizedBox(width: 16),
+                      Container(color: Colors.red, width: 60, height: 55),
+                    ],
+                  ),
+                ),
+              ], //fin del children widget de column widget[]
+            ), //fin de column
+          ) //fin de container
+          ), //fin de padding
+    ); //fin descaffold
+  } //fin widget
+} //finpaginadeinicio
+
